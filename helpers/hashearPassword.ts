7d831e04a80d1826a849fcd48f4baf523443e4f1
UGALDEMMJ <ugaldemoralesmj@gmail.com>
2025-06-23 22:19:30 -0600

    .

--- helpers/hashearPassword.ts
+++ helpers/hashearPassword.ts
@@ -1,12 +1,15 @@
-import * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
+import * as _bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
 
-export async function hashPassword(normalPassword:string): Promise<string> {
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(normalPassword, salt);
-    return hash;
+export async function hashPassword(normalPassword: string): Promise<string> {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(normalPassword);
+    const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
 }
 
-export async function verifyPassword(normalPassword:string, hashedPassword:string): Promise<boolean> {
-    return await bcrypt.compareSync(normalPassword, hashedPassword);
+export async function verifyPassword(normalPassword: string, hashedPassword: string): Promise<boolean> {
+    const hash = await hashPassword(normalPassword);
+    return hash === hashedPassword;
 }
 
