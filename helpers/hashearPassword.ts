e82a9a0e82b69ba7ed13c36620e16e4a958a09b8
UGALDEMMJ <ugaldemoralesmj@gmail.com>
2025-06-23 22:10:57 -0600

    Fixing hashing problems and import issues

--- helpers/hashearPassword.ts
+++ helpers/hashearPassword.ts
@@ -1,11 +1,11 @@
 import bcrypt from "https://deno.land/x/bcryptjs@2.4.3/mod.ts";
 
-export async function hashPassword(normalPassword: string): Promise<string> {
+export function hashPassword(normalPassword: string): Promise<string> {
     // bcryptjs es síncrono, pero puedes envolverlo en Promise para mantener la API
     return Promise.resolve(bcrypt.hashSync(normalPassword, 10));
 }
 
-export async function verifyPassword(normalPassword: string, hashedPassword: string): Promise<boolean> {
+export function verifyPassword(normalPassword: string, hashedPassword: string): Promise<boolean> {
     return Promise.resolve(bcrypt.compareSync(normalPassword, hashedPassword));
 }
 
